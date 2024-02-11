c4704a43adadd6664522ca6dbec181cc0c81bffb
Emre Eroğlu <emre1eroglu7@gmail.com>
2024-02-11 15:16:34 +0300

    last revize

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,15 @@ function hambOff() {
     document.querySelector(".hamb-icon-open").style.display = "block";
     document.querySelector(".hamb-icon-off").style.display = "none";
 }
+
+const goTopBtn = document.querySelector(".go-top-btn");
+window.addEventListener('scroll', checkHeight);
+function checkHeight() {
+    if (window.scrollY < 200) {
+        goTopBtn.style.opacity = "0";
+    }
+    else {
+        goTopBtn.style.opacity = "1";
+    }
+
+}
